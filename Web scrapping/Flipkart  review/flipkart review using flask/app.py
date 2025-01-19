d4a2ad804135db23c17056c45f37f2a41b029ddc
MoraXX-D <shubham.21dec03@gmail.com>
2025-01-19 23:56:41 +0530

    web scrapping on flask

--- Web scrapping/Flipkart  review/flipkart review using flask/app.py
+++ Web scrapping/Flipkart  review/flipkart review using flask/app.py
@@ -1,1 +1,6 @@
 """Hosting the same flipkart review using web scrapping by flask"""
+from flask import Flask, render_template, request
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as uOpen
+import requests
+import logging
