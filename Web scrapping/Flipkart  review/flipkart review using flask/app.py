4bd679742cd10ede731b3e5e342d93615528e4bb
MoraXX-D <shubham.21dec03@gmail.com>
2025-01-22 23:48:48 +0530

    web scrapping

--- Web scrapping/Flipkart  review/flipkart review using flask/app.py
+++ Web scrapping/Flipkart  review/flipkart review using flask/app.py
@@ -1,6 +1,15 @@
 """Hosting the same flipkart review using web scrapping by flask"""
+
 from flask import Flask, render_template, request
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uOpen
 import requests
 import logging
+
+base_url = "https://www.flipkart.com/search?q="
+
+app = Flask(__name__)
+logging.basicConfig(level=logging.INFO)
+
+url_client = uReq(flipkart_url)
+flipkart_page = url_client.read()
